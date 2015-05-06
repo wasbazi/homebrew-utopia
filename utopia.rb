56ada9d99ad8f76b2db4e0d3235535c8c6230254
Austin Riendeau <ariendeau@itriagehealth.com>
2015-05-06 13:29:47 -0600

    releasing version 0.8.0

--- utopia.rb
+++ utopia.rb
@@ -4,10 +4,10 @@ class Utopia < Formula
   homepage 'http://www.utopianapp.com'
 
   stable do
-    version '0.7.2'
+    version '0.8.0'
     @@file = "utopia_#{version}_darwin_amd64"
-    url "https://s3.amazonaws.com/aetna-utopia/#{version}/utopia-deploy_#{version}_darwin_amd64.tar.gz"
-    sha1 '0dd9c48ee767100259e5559166229e01b98c6fa5'
+    url "https://s3.amazonaws.com/aetna-utopia/#{version}/utopia_#{version}_darwin_amd64.tar.gz"
+    sha1 '4639f4cd6dad9e7716c7f937208e45e29e0d4175'
   end
 
   devel do
@@ -30,7 +30,7 @@ class Utopia < Formula
     PROG=utopia source #{HOMEBREW_PREFIX}/etc/bash_completion.d/utopia_autocomplete.sh
 
   To enable zsh completion, you will need to add this your ./zshrc:
-    PROG=utopia source #{HOMEBREW_PREFIX}/share/zsh/site-functions/utopia_autocomplete.sh
+    PROG=utopia source #{HOMEBREW_PREFIX}/share/zsh/site-functions/utopia_autocomplete.zsh
   [0;36m==>[0m[1;37m Completion[0m
   EOS
   end
