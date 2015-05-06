a0c172b3ee126657f80789a347c6f25a6f784c25
Austin Riendeau <ariendeau@itriagehealth.com>
2015-05-06 11:48:43 -0600

    releasing version 0.8.0

--- utopia.rb
+++ utopia.rb
@@ -13,7 +13,7 @@ class Utopia < Formula
   devel do
     version '0.8.0'
     url "https://s3.amazonaws.com/aetna-utopia/#{version}/utopia_#{version}_darwin_amd64.tar.gz"
-    sha1 '83e336038dd52127d25d57641f6ae25b393c0663 releaes.txt'
+    sha1 'dd558c80a830613d4fae780d6646fe560f724918 releaes.txt'
   end
 
   depends_on :arch => :intel
