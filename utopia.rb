2d1d11a75d6df97597bc31bf35620512c02c237e
root <utopia@itriagehealth.com>
2015-11-25 21:23:44 +0000

    releasing version 0.11.8

--- utopia.rb
+++ utopia.rb
@@ -7,7 +7,7 @@ class Utopia < Formula
 	version '0.11.8'
 	@@file = "utopia_#{version}_darwin_amd64"
 	url "https://s3.amazonaws.com/aetna-utopia/#{version}/utopia_#{version}_darwin_amd64.tar.gz"
-	sha1 'f52ff36949e3bafbf74c421e8dad86605a6b7ef7'
+	sha1 '64fe959272298f177ad85d50543929941e97d216'
   end
 
   devel do
@@ -20,18 +20,6 @@ class Utopia < Formula
 
   def install
 	bin.install "utopia_#{version}_darwin_amd64"
-	bash_completion.install "autocomplete/utopia_autocomplete.sh"
-	zsh_completion.install "autocomplete/utopia_autocomplete.zsh"
 	system "mv", bin/"utopia_#{version}_darwin_amd64", bin/"utopia"
-  end
-
-  def caveats; <<-EOS.undent
-  To enable bash completions, you will need to add this to you .bashrc:
-	PROG=utopia source #{HOMEBREW_PREFIX}/etc/bash_completion.d/utopia_autocomplete.sh
-
-  To enable zsh completion, you will need to add this your ./zshrc:
-	PROG=utopia source #{HOMEBREW_PREFIX}/share/zsh/site-functions/utopia_autocomplete.zsh
-  [0;36m==>[0m[1;37m Completion[0m
-  EOS
   end
 end
