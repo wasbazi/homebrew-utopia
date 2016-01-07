7c9a69870ae76c1830cef8692ea301b80be3f64c
Avi W-H <wasbazi@gmail.com>
2016-01-07 13:56:46 -0700

    releasing version devel-01072016-135557

--- utopia.rb
+++ utopia.rb
@@ -12,9 +12,9 @@ class Utopia < Formula
 
   
   devel do
-    version 'devel-01072016-135324'
+    version 'devel-01072016-135557'
     url "https://s3.amazonaws.com/aetna-utopia/devel/utopia_#{version}_darwin_amd64.tar.gz"
-    sha1 '0f89a1de59d19dc692302d0e24ba14b848686ff3'
+    sha1 '71cc1ddbd911f9bb1f27ccecb2bd263b0f128d8a'
   end
 
   depends_on :arch => :intel
